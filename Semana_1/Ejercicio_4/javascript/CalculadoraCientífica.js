f028995cdff672b5c38562de2dc98860dae4acd8
Ángel Olmedo García <angelixus99@gmail.com>
2019-12-05 10:02:04 +0100

    dewdew

--- Semana_1/Ejercicio_4/javascript/CalculadoraCientífica.js
+++ Semana_1/Ejercicio_4/javascript/CalculadoraCientífica.js
@@ -151,7 +151,7 @@ scientificCalc.processStack = function(input) {
 };
 
 scientificCalc.showValueOnInput = function(id, text) {
-    //document.getElementById(id).value = text;
+    document.getElementById(id).value = text;
 };
 
 scientificCalc.deleteLast = function() {
