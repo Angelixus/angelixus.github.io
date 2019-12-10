e210cf8f556b2b8c17ff3ced4dcd9a28a0ba96f7
Ángel Olmedo García <angelixus99@gmail.com>
2019-12-10 20:00:30 +0100

    ewfewf
    fewefw

--- Semana_2/Ejercicio_10/javascript/Ejercicio10.js
+++ Semana_2/Ejercicio_10/javascript/Ejercicio10.js
@@ -1,41 +1,40 @@
 class Converter {
   constructor(htmlwrapper) {
-    this.api = "http://data.fixer.io/api/latest?access_key=fb1bbcdec0121b41137b4efd51b7c6e2&symbols=USD,AUD,CAD,PLN,MXN&format=1";
-    this.htmlwrapper = htmlwrapper
+    this.api =
+      "http://data.fixer.io/api/latest?access_key=fb1bbcdec0121b41137b4efd51b7c6e2&symbols=USD,AUD,CAD,PLN,MXN&format=1";
+    this.htmlwrapper = htmlwrapper;
   }
 
   convert(inputVal) {
     try {
-      var eurVal = parseFloat(this.htmlwrapper.readFromHtml(inputVal))
+      var eurVal = parseFloat(this.htmlwrapper.readFromHtml(inputVal));
 
       var objectReference = this;
       fetch(this.api)
-      .then(function(response) {
-        let data = response.json();
-        return data;
-      })
-      .then(function(data) {
-        objectReference.htmlwrapper.writeToHTML('#usdOut', eurVal * data.rates.USD)
-        objectReference.htmlwrapper.writeToHTML('#audOut', eurVal * data.rates.AUD)
-        objectReference.htmlwrapper.writeToHTML('#cadOut', eurVal * data.rates.CAD)
-        objectReference.htmlwrapper.writeToHTML('#plnOut', eurVal * data.rates.PLN)
-        objectReference.htmlwrapper.writeToHTML('#mxnOut', eurVal * data.rates.MXN)
-      })
-    } catch(e) {
-      this.htmlwrapper.writeToHTML('#eurInput', '')
-      this.htmlwrapper.writeToHTML('#usdOut', '')
-      this.htmlwrapper.writeToHTML('#audOut', '')
-      this.htmlwrapper.writeToHTML('#cadOut', '')
-      this.htmlwrapper.writeToHTML('#plnOut', '')
-      this.htmlwrapper.writeToHTML('#mxnOut', '')
+        .then(function(response) {
+          let data = response.json();
+          return data;
+        })
+        .then(function(data) {
+          $("#usdOut").text(eurVal * data.rates.USD);
+          $("#audOut").text(eurVal * data.rates.AUD);
+          $("#cadOut").text(eurVal * data.rates.CAD);
+          $("#plnOut").text(eurVal * data.rates.PLN);
+          $("#mxnOut").text(eurVal * data.rates.MXN);
+        });
+    } catch (e) {
+      $("#eurInput").text("");
+      $("#usdOut").text("");
+      $("#audOut").text("");
+      $("#cadOut").text("");
+      $("#plnOut").text("");
+      $("#mxnOut").text("");
     }
   }
 }
 
 class HtmlWrapper {
-  constructor() {
-
-  }
+  constructor() {}
 
   writeToHTML(id, toWrite) {
     document.getElementById(id).value = toWrite;
