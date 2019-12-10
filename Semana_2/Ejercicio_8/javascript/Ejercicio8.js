ccf30550f6514e43a4213587ac3a529ab3ce5351
Ángel Olmedo García <angelixus99@gmail.com>
2019-12-10 06:27:23 +0100

    fewfew

--- Semana_2/Ejercicio_8/javascript/Ejercicio8.js
+++ Semana_2/Ejercicio_8/javascript/Ejercicio8.js
@@ -14,47 +14,54 @@ class WeatherGetter {
       this.lat.toString() +
       "&lon=" +
       this.long.toString() +
-      '&units=metric&appid=4a4e634754d6993e73a82028d2d7c60e';
+      "&units=metric&appid=4a4e634754d6993e73a82028d2d7c60e";
 
     this.getWeather();
   }
 
   getWeather() {
-      var objectReference = this;
-      fetch(this.weatherApi)
-        .then(function(response) {
-            let data = response.json();
-            return data;
-        })
-        .then(function(data) {
-            objectReference.temperature = Math.round(data.main.temp);
-            objectReference.description = data.weather[0].description;
-            objectReference.iconId = data.weather[0].icon;
-            objectReference.cityCountry = data.name + ',' + data.sys.country;
-        }).then(function() {
-            var selectedName = ""
-            $('#citySelect > option').each(function() {
-                if($(this).prop('selected') == true) {
-                    selectedName = $(this).prop('value')
-                }
-            })
+    var objectReference = this;
+    fetch(this.weatherApi)
+      .then(function(response) {
+        let data = response.json();
+        return data;
+      })
+      .then(function(data) {
+        objectReference.temperature = Math.round(data.main.temp);
+        objectReference.description = data.weather[0].description;
+        objectReference.iconId = data.weather[0].icon;
+        objectReference.cityCountry = data.name + "," + data.sys.country;
+      })
+      .then(function() {
+        var selectedName = "";
+        $("#citySelect > option").each(function() {
+          if ($(this).prop("selected") == true) {
+            selectedName = $(this).prop("value");
+          }
+        });
 
-            if(selectedName == objectReference.optionName) {
-                objectReference.showWeather()
-            }
-        })
+        if (selectedName == objectReference.optionName) {
+          objectReference.showWeather();
+        }
+      });
   }
 
   showWeather() {
-    $('.weatherImage').attr('src', 'icons/' + this.iconId + '.png')
-    $('.temperature-value').text(this.temperature.toString())
-    $('.temperature-description').text(this.description)
-    $('.place').text(this.cityCountry)
+    console.log("Showing Weather");
+    console.log(this.temperature);
+    console.log(this.description);
+    console.log(this.iconId);
+    console.log(this.cityCountry);
+
+    $(".weatherImage").attr("src", "icons/" + this.iconId + ".png");
+    $(".temperature-value").text(this.temperature.toString() + 'ºC');
+    $(".temperature-description").text(this.description);
+    $(".place").text(this.cityCountry);
   }
 }
 
-var londonGetter = new WeatherGetter('london', 51.5085, -0.1258);
-var tokyoGetter = new WeatherGetter('tokyo', 35.6828, 139.759);
-var romeGetter = new WeatherGetter('rome', 41.8933, 12.4829);
-var madridGetter = new WeatherGetter('madrid', 40.4167, -3.7036);
-var newyorkGetter = new WeatherGetter('newyork', 40.7306, -73.9867);
+var londonGetter = new WeatherGetter("london", 51.5085, -0.1258);
+var tokyoGetter = new WeatherGetter("tokyo", 35.6828, 139.759);
+var romeGetter = new WeatherGetter("rome", 41.8933, 12.4829);
+var madridGetter = new WeatherGetter("madrid", 40.4167, -3.7036);
+var newyorkGetter = new WeatherGetter("newyork", 40.7306, -73.9867);
