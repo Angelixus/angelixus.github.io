57ba81d5e5beadbc3260b75ffc77faea85a5e5f0
Ángel Olmedo García <angelixus99@gmail.com>
2019-12-10 20:27:35 +0100

    few

--- Semana_2/Ejercicio_10/javascript/Ejercicio10.js
+++ Semana_2/Ejercicio_10/javascript/Ejercicio10.js
@@ -12,20 +12,20 @@ class Converter {
         url: objectReference.api,
         dataType: "json",
         success: function(json){
-          $("#usdOut").text(eurVal * json.rates.USD);
-          $("#audOut").text(eurVal * json.rates.AUD);
-          $("#cadOut").text(eurVal * json.rates.CAD);
-          $("#plnOut").text(eurVal * json.rates.PLN);
-          $("#mxnOut").text(eurVal * json.rates.MXN);        
+          $("#usdOut").val(eurVal * json.rates.USD);
+          $("#audOut").val(eurVal * json.rates.AUD);
+          $("#cadOut").val(eurVal * json.rates.CAD);
+          $("#plnOut").val(eurVal * json.rates.PLN);
+          $("#mxnOut").val(eurVal * json.rates.MXN);        
         }
     });
     } catch (e) {
-      $("#eurInput").text("");
-      $("#usdOut").text("");
-      $("#audOut").text("");
-      $("#cadOut").text("");
-      $("#plnOut").text("");
-      $("#mxnOut").text("");
+      $("#eurInput").val("");
+      $("#usdOut").val("");
+      $("#audOut").val("");
+      $("#cadOut").val("");
+      $("#plnOut").val("");
+      $("#mxnOut").val("");
     }
 
     
