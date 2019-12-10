494635cf40f5e488d16165275e408f8a06b21ca3
Ángel Olmedo García <angelixus99@gmail.com>
2019-12-10 04:01:01 +0100

    fewee

--- Semana_2/Ejercicio_7/javascript/Ejercicio7.js
+++ Semana_2/Ejercicio_7/javascript/Ejercicio7.js
@@ -37,6 +37,7 @@ class JQueryWrapper {
 
   showAllHTMLElements(parentToAppend) {
     $('html').find('*').each(function() {
+      $(parentToAppend).append('<p>Elemento: ' + this.nodeName + ' Padre: ' + this.parent().nodeName + '</p>')
       alert(this.nodeName);
     })
   }
