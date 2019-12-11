76d5084570575ca1e09247f96431e09905761752
Ángel Olmedo García <angelixus99@gmail.com>
2019-12-11 04:18:30 +0100

    few

--- Semana_3/Ejercicio_13/javascript/Ejercicio13.js
+++ Semana_3/Ejercicio_13/javascript/Ejercicio13.js
@@ -6,9 +6,9 @@ class FileInfoShow {
   readFiles(eventObject) {
     var file = eventObject.target.files[0]
 
-    $('#fileName').val('Nombre del Archivo: ' + file.name)
-    $('#fileSize').val('Tamaño del Archivo: ' + file.size + ' bytes')
-    $('#fileType').val('Tipo del Archivo: ' + file.type)
+    $('#fileName').text('Nombre del Archivo: ' + file.name)
+    $('#fileSize').text('Tamaño del Archivo: ' + file.size + ' bytes')
+    $('#fileType').text('Tipo del Archivo: ' + file.type)
   }
 }
 
