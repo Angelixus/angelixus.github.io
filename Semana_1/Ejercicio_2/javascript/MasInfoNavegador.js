d4d0ba2471616338e63e11267dbc89122ab65b27
Ángel Olmedo García <angelixus99@gmail.com>
2019-12-11 05:41:05 +0100

    few

--- Semana_1/Ejercicio_2/javascript/MasInfoNavegador.js
+++ Semana_1/Ejercicio_2/javascript/MasInfoNavegador.js
@@ -1,10 +1,35 @@
+class HTMLWriterSecond {
+    constructor(tagId, toWrite) {
+        this.tagId = tagId;
+        this.toWrite = toWrite;
+    }
+
+    writeToHTML() {
+        console.log(this.tagId);
+        var htmlTag = document.getElementById(this.tagId);
+        if(htmlTag != null) {
+            htmlTag.textContent += this.toWrite;
+        }
+    }
+
+    setTagId(tagId) {
+        this.tagId = tagId;
+    }
+
+    setTextToWrite(toWrite) {
+        this.toWrite = toWrite;
+    }
+}
+
+var htmlWriterSecond = new HTMLWriterSecond()
+
 var restOfData = wholeNavInfo.getRestOfData();
 var tagId = "parrafos";
 
-htmlWriter.setTagId(tagId);
+htmlWriterSecond.setTagId(tagId);
 
 for(var index in restOfData) {
     var text = restOfData[index] + ' ';
-    htmlWriter.setTextToWrite(text);
-    htmlWriter.writeToHTML();
+    htmlWriterSecond.setTextToWrite(text);
+    htmlWriterSecond.writeToHTML();
 }
