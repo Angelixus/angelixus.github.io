c2d0aa23aac44343c3d8e6414d1907ee75b85d7b
Ángel Olmedo García <angelixus99@gmail.com>
2019-12-11 01:29:07 +0100

    fe

--- Semana_3/Ejercicio_12/javascript/Ejercicio12.js
+++ Semana_3/Ejercicio_12/javascript/Ejercicio12.js
@@ -1,36 +1,36 @@
 class GeoLocalizacion {
-    constructor() {
-        this.lat = 0;
-        this.long = 0;
+  constructor() {
+    this.lat = 0;
+    this.long = 0;
 
-        this.getUserPos()
-    }
+    this.getUserPos();
+  }
 
-    getUserPos() {
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this.locationGetSuccess, this.locationGetError)
-        } else {
-            alert("La geolocalización no esta disponible en este navegador");
-        }
-    }
+  getUserPos() {
+    if (navigator.geolocation) {
+      var objectReference = this;
+      navigator.geolocation.getCurrentPosition(
+        function(position) {
+          alert("Localización obtenida satisfactoriamente");
 
-    locationGetSuccess(position) {
-        alert('Localización obtenida satisfactoriamente')
+          objectReference.lat = position.coords.latitude;
+          objectReference.long = position.coords.longitude;
 
-        this.lat = position.coords.latitude;
-        this.long = position.coords.longitude;
-
-        $('#latitudeP').text('Latitud: ' + objectReference.lat.toString())
-        $('#longitudeP').text('Longitud: ' + objectReference.long.toString())
-    }
-
-    locationGetError(error) {
-        if(err.code == 1) {
+          $("#latitudeP").text("Latitud: " + objectReference.lat.toString());
+          $("#longitudeP").text("Longitud: " + objectReference.long.toString());
+        },
+        function(error) {
+          if (error.code == 1) {
             alert("Error: No se ha permitido el acceso a la geolocalización");
-         } else if( err.code == 2) {
+          } else if (error.code == 2) {
             alert("Error: La geolocalización no esta disponible");
-         }
+          }
+        }
+      );
+    } else {
+      alert("La geolocalización no esta disponible en este navegador");
     }
+  }
 }
 
-var gelocation = new GeoLocalizacion()
+var gelocation = new GeoLocalizacion();
