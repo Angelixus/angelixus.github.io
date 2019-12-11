9b9a1cd7a4169ba2ca199184aa48875706e4c72d
Ángel Olmedo García <angelixus99@gmail.com>
2019-12-11 04:45:03 +0100

    ffrre

---
+++ Semana_3/Ejercicio_14/javascript/MapaKML.js
@@ -0,0 +1,34 @@
+class GeoLocalizacion {
+  constructor() {
+    this.googleStaticApi = "";
+
+    initMap();
+    
+  }
+
+  initMap() {
+    var src = 'archivos/1.kml'
+    var pos = { lat: 43.027502, lng: -6.246995 };
+    var map = new google.maps.Map(document.getElementById("actualMap"), {
+      center: pos,
+      zoom: 8,
+    });
+
+    var kmlLayer = new google.maps.KmlLayer(src, {
+      suppressInfoWindows: true,
+      preserveViewport: false,
+      map: map
+    });
+
+    /*
+    kmlLayer.addListener('click', function(event) {
+      var content = event.featureData.infoWindowHtml;
+      var testimonial = document.getElementById('capture');
+      testimonial.innerHTML = content;
+    });
+    */
+
+  }
+}
+
+var geolocation = new GeoLocalizacion();
