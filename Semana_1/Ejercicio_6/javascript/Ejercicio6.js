bd80a329a3c022bdaf1e77c3b80649b22257b022
Ángel Olmedo García <angelixus99@gmail.com>
2019-12-11 06:21:32 +0100

    gr

--- Semana_1/Ejercicio_6/javascript/Ejercicio6.js
+++ Semana_1/Ejercicio_6/javascript/Ejercicio6.js
@@ -1,16 +1,16 @@
 class EURToUSDConverter {
-    constructor() {
+  constructor() {}
 
-    }
+  convert() {
+    var num = parseFloat(document.getElementById("eurInput").value);
 
-    convert() {
-        try {
-            document.getElementById('usdOut').value = parseFloat(document.getElementById('eurInput').value * 1.1077);
-        } catch(e) {
-            document.getElementById('eurInput').value = '';
-            document.getElementById('usdOut').value = '';
-        }
+    if (isNaN(num)) {
+      document.getElementById("eurInput").value = "";
+      document.getElementById("usdOut").value = "";
+    } else {
+      document.getElementById("usdOut").value = (num * 1.1077).toFixed(2);
     }
+  }
 }
 
 var converter = new EURToUSDConverter();
