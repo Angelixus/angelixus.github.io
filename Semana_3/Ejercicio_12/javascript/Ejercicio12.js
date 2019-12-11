d33c52153ca0b5ebd6228f070811992397b0b2e7
Ángel Olmedo García <angelixus99@gmail.com>
2019-12-11 01:03:34 +0100

    fwefew

---
+++ Semana_3/Ejercicio_12/javascript/Ejercicio12.js
@@ -0,0 +1,24 @@
+class GeoLocalizacion {
+    constructor() {
+        this,geolocation = navigator.geolocation;
+        this.lat = 0;
+        this.long = 0;
+    }
+
+    getUserPos() {
+        var objectReference = this;
+        this.geolocation.getCurrentPosition(function(position) {
+            objectReference.lat = position.coords.latitude;
+            objectReference.long = position.coords.longitude;
+
+            $('#latitudeP').text('Latitud: ' + objectReference.lat.toString())
+            $('#longitudeP').text('Longitud: ' + objectReference.long.toString())
+        }, function(error) {
+            if(err.code == 1) {
+                alert("Error: No se ha permitido el acceso a la geolocalización");
+             } else if( err.code == 2) {
+                alert("Error: La geolocalización no esta disponible");
+             }
+        })
+    }
+}
